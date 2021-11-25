59ebfb34e9c5c2e1b4504bd56dd8acd6b50d17e1
Omar Valdivia Samudio <omarvalsam@gmail.com>
2021-11-24 18:58:39 -0700

    added loading code

--- assets/js/script.js
+++ assets/js/script.js
@@ -215,8 +215,56 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+  // get tasks items from localStorage
+  var tasks = localStorage.getItem('tasks');
+  if (tasks === null) {
+    tasks = [];
+  }
+
+  // converts tasks from the string format back into an array of objects
+  tasks = JSON.parse(tasks);
+
+  // iterates through a task array and creates task elements on the page from it
+  for (var i = 0; i < tasks.length; i++){
+    tasks[i].id = taskIdCounter;
+    
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    
+    var taskActionsEl = createTaskActions(tasks[i].id)
+
+    listItemEl.append(taskInfoEl);
+    listItemEl.append(taskActionsEl);
+    
+    if (tasks[i].status === "to do"){
+      listItemEl.querySelector("select[name='status-change']").selectIndex = 0;
+      tasksToDoEl.append(listItemEl)
+    }
+    else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector("select[name='status-change']").selectIndex = 1;
+      tasksInProgressEl.append(listItemEl);
+    }
+    else if (tasks[i].status === "complete") {
+      listItemEl.querySelector("select[name='status-change']").selectIndex = 2;
+      tasksCompletedEl.append(listItemEl)
+    }
+    taskIdCounter++;
+    saveTasks();
+    console.log(listItemEl);
+    
+  }
+}
+
 formEl.addEventListener("submit", taskFormHandler);
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
